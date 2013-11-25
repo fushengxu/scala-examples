b510f20c22922bddca988c1b6abe147f343af7d1
Nadav Dav <nadavik@gmail.com>
2013-11-26 00:26:52 +0200

    Added contact info to site meta

--- CakePatternBuilder.scala
+++ CakePatternBuilder.scala
@@ -1,16 +1,22 @@
 case class User (name:String)
 case class Site (name:String, user: User, metaData: SiteMetaData)
-case class SiteMetaData (color: String, id: String)
+case class SiteMetaData (color: String, id: String, contactInfo: ContactInfo)
+case class ContactInfo (email: String)
 
 trait UserProvider {
   def userName = "mike"
   def user: User = User(userName)
 }
 
-trait SiteMetaDataProvider {
+trait ContactInfoProvider {
+  def contactInfoEmail = "my@email.com"
+  def contactInfo = ContactInfo(contactInfoEmail)
+}
+
+trait SiteMetaDataProvider extends ContactInfoProvider{
   def siteMetaColor = "black"
   def siteMetaId = "1234-5678"
-  def siteMetaData: SiteMetaData = SiteMetaData(siteMetaColor, siteMetaId)
+  def siteMetaData: SiteMetaData = SiteMetaData(siteMetaColor, siteMetaId, contactInfo)
 }
 
 trait SiteProvider extends UserProvider with SiteMetaDataProvider{
@@ -28,6 +34,7 @@ object test{
     val story = new Story{
       override val siteName = "my other site"
       override val userName = "cool keith"
+      override val contactInfoEmail = "eeeeemail@ho.yeah"
     }
     val site = story.site
     println(site)
