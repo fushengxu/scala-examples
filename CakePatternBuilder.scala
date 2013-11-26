2599d8ccbd10242afcb0812bd879676ed3e0ee21
Nadav Dav <nadavik@gmail.com>
2013-11-26 19:18:29 +0200

    added scopes to each provider

--- CakePatternBuilder.scala
+++ CakePatternBuilder.scala
@@ -4,41 +4,51 @@ case class SiteMetaData (color: String, id: String, contactInfo: ContactInfo)
 case class ContactInfo (email: String)
 
 trait UserProvider {
-  def userName = "mike"
-  def user: User = User(userName)
+  import user._
+  protected object user {
+    var name = "mike"
+  }
+  def aUser = User( name )
 }
 
 trait ContactInfoProvider {
-  def contactInfoEmail = "my@email.com"
-  def contactInfo = ContactInfo(contactInfoEmail)
+  import contactInfo._
+  protected object contactInfo {
+    var email = "my@email.com"
+  }
+  def aContactInfo = ContactInfo(email)
 }
 
 trait SiteMetaDataProvider extends ContactInfoProvider{
-  def siteMetaColor = "black"
-  def siteMetaId = "1234-5678"
-  def siteMetaData: SiteMetaData = SiteMetaData(siteMetaColor, siteMetaId, contactInfo)
+  import siteMetaData._
+  object siteMetaData {
+    var color = "black"
+    var id = "1234-5678"
+  }
+  def aSiteMetaData = SiteMetaData( color, id, aContactInfo)
 }
 
 trait SiteProvider extends UserProvider with SiteMetaDataProvider{
-  def siteName = "my site"
-  def site: Site = Site(siteName, user, siteMetaData)
+  import site._
+  object site {
+    var name = "my site"
+  }
+  def aSite = Site( name, aUser, aSiteMetaData )
 }
 
 trait Story extends SiteProvider
-trait StoryAboutSiteWithNoId extends Story{
-  override val siteMetaId = "no id :("
-}
 
 object test{
-  def main (args : Array[String]) {
-    val story = new Story{
-      override val siteName = "my other site"
-      override val userName = "cool keith"
-      override val contactInfoEmail = "eeeeemail@ho.yeah"
-    }
-    val site = story.site
-    println(site)
-    println((new StoryAboutSiteWithNoId {}).site)
 
+  val story = new Story {
+    user.name = "cool keith"
+    contactInfo.email = "eeeeemail@ho.yeah"
+    site.name = "my other site"
+  }
+  import story._
+
+  def main (args : Array[String]) {
+    val site = aSite
+    println(site)
   }
 }
