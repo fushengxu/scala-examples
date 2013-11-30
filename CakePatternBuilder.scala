5123dab50f8d82a43dbd3cc3296777ae1b9aa456
Nadav Dav <nadavik@gmail.com>
2013-11-30 13:25:11 +0200

    Update CakePatternBuilder.scala

--- CakePatternBuilder.scala
+++ CakePatternBuilder.scala
@@ -3,52 +3,55 @@ case class Site (name:String, user: User, metaData: SiteMetaData)
 case class SiteMetaData (color: String, id: String, contactInfo: ContactInfo)
 case class ContactInfo (email: String)
 
-trait UserProvider {
-  import User._
-  protected object User {
+trait UserStory {
+  import aboutUser._
+  protected object aboutUser {
     var name = "mike"
   }
   def user = User( name )
 }
 
-trait ContactInfoProvider {
-  import ContactInfo._
-  protected object ContactInfo {
+trait ContactInfoStory {
+  import aboutContactInfo._
+  protected object aboutContactInfo {
     var email = "my@email.com"
   }
   def contactInfo = ContactInfo(email)
 }
 
-trait SiteMetaDataProvider extends ContactInfoProvider{
-   protected object SiteMetaData {
+trait SiteMetaDataStory extends ContactInfoStory{
+   protected object aboutSiteMetaData {
     var color = "black"
     var id = "1234-5678"
   }
-  import SiteMetaData._
+  import aboutSiteMetaData._
   def siteMetaData = SiteMetaData( color, id, contactInfo)
 }
 
-trait SiteProvider extends UserProvider with SiteMetaDataProvider{
-  import Site._
-  object Site {
+trait SiteStory extends UserStory with SiteMetaDataStory{
+  import aboutSite._
+  object aboutSite {
     var name = "my site"
   }
   def site = Site( name, user, siteMetaData )
 }
 
-trait Story extends SiteProvider
-
-object main{
-
-  val story = new Story {
-    SiteMetaData.color
-    User.name = "cool keith"
-    ContactInfo.email = "eeeeemail@ho.yeah"
-    Site.name = "my other site"
+object StoryBuilder {
+  val story = new SiteStory {
+    aboutSiteMetaData color = "blue"
+    aboutUser name = "cool keith"
+    aboutContactInfo email = "eeeeemail@ho.yeah"
+    aboutSite name = "my other site"
   }
 
   def main (args : Array[String]) {
-    val site = site
+    val site = story.site
+    story.user
+    println(site)
+  }
+}
+    val site = story.site
+    story.user
     println(site)
   }
 }
