fa37df9bf1d5a5ca34804eb3d4ca91171aff8125
Nadav Dav <nadavik@gmail.com>
2013-12-08 09:48:03 +0200

    Create RunningNativeProcess.scala

---
+++ RunningNativeProcess.scala
@@ -0,0 +1,8 @@
+object execute{
+  def main(args: Array[String]) {
+    import scala.sys.process._
+    val lines2 = "ping 8.8.8.8".lines_!
+    lines2.foreach( println(_))
+    println("OK")
+  }
+}
